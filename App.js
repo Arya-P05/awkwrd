8a2f8c4acb530d6ec68d151cbd50fceb9dee7582
Arya <jackrack040@gmail.com>
2025-03-13 11:28:54 -0400

    wipe out everything

--- App.js
+++
@@ -1,5 +0,0 @@
-
-// This is the entry point for Expo
-import App from './src/App.native';
-
-export default App;
--- src/index.native.js
+++
@@ -1,6 +0,0 @@
-
-import { registerRootComponent } from 'expo';
-import App from './App.native';
-
-// Register the app as the main component
-registerRootComponent(App);
